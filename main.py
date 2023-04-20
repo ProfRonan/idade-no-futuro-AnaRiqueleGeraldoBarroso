af9b658160dd5f797aaef9855c1fb86cfa8173c8
Ana Riquele <riquelegeraldo@gmail.com>
2023-04-20 14:05:00 -0300

    Versão com a vírgula

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ novo_ano = int(input("Digite outro ano: "))
 nome = str(input("Digite seu nome: "))
 ano_nascimento = ano_atual - idade
 nova_idade = novo_ano - ano_nascimento
-print(nome,"no ano de", novo_ano, "você terá", nova_idade, "anos")
+print(f'{nome}, no ano de {novo_ano} você terá {nova_idade} anos')
